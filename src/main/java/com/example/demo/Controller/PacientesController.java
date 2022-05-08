478cd1acc26c810448d30d99a0a47103d84ffa47
Célita Morales <cmoralesm37@miumg.edu.gt>
2022-05-08 04:41:13 -0600

    primera parte

---
+++ src/main/java/com/example/demo/Controller/PacientesController.java
@@ -0,0 +1,46 @@
+package com.example.demo.Controller;
+
+
+import com.example.demo.Entidades.Pacientes;
+import com.example.demo.Services.PacientesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacientesController {
+     @Autowired
+    PacientesService pacientesService;
+
+     @GetMapping
+     public List<Pacientes> Listar(){
+         return pacientesService.findAll();
+     }
+    @GetMapping(value = "/{id}")
+     public Pacientes obtenerPacientes(@PathVariable Long id){
+         return pacientesService.findById(id);
+     }
+
+     @PutMapping("/{id}")
+    public void modifyPaciente(@PathVariable Long id, @RequestBody Pacientes pacientes)
+     {
+         pacientesService.modifyPaciente(id, pacientes);
+     }
+
+    @DeleteMapping("/{id}")
+    public void deletePaciente(@PathVariable Long id){
+         pacientesService.deletePaciente(id);
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+insert into pacientes (nombre, segundonombre, apellido, segundoapellido, edad) values ('Ana', 'María','Lucero','Escobar',85)
